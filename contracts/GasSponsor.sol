3612609e7fc900ced733b51e77a96520a95da4f9
TURK-20 <admin@kriptoalsat.online>
2025-10-29 03:39:07 +0300

    Update GasSponsor.sol

--- contracts/GasSponsor.sol
+++ contracts/GasSponsor.sol
@@ -1,1 +1,12 @@
-# FeePool (native coin bakiyesi) + relayer’a ödemeler
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title GasSponsor (TURK-20 Gas Havuzu)
+/// @notice Relayer'ların gas ücretlerini FeePool'dan karşılar.
+interface IGasSponsor {
+    event Sponsored(address indexed user, address target, bytes4 selector, uint256 gasCostWei);
+
+    function setForwarder(address forwarder) external;
+    function setFeeManager(address fm) external;
+    function sponsorCall(bytes calldata forwardRequest, bytes calldata signature) external payable;
+}
