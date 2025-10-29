b7996e4d5642df36180562209acd62e484d81608
TURK-20 <admin@kriptoalsat.online>
2025-10-29 03:38:46 +0300

    Update FeeManager.sol

--- contracts/FeeManager.sol
+++ contracts/FeeManager.sol
@@ -1,1 +1,12 @@
-# lisans kontrolü + günlük işlem kotası (free=1000/gün; pro=∞)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title FeeManager (TURK-20 Sponsor Gas Kota Yönetimi)
+/// @notice Lisanslı kullanıcıların günlük ücretsiz işlem kotasını izler.
+interface IFeeManager {
+    event QuotaUsed(address indexed user, uint256 dayIndex, uint256 used, uint256 max);
+
+    function setDailyQuota(uint256 freeQ, uint256 proQ) external;
+    function canSponsor(address user) external view returns (bool);
+    function consumeQuota(address user) external;
+}
